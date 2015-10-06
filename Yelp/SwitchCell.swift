135609729adad257de489ed98303df2147bd71c8
Yayang Tian <ytian@walmartlabs.com>
2015-10-05 23:51:33 -0700

    Added filter delegate.

--- Yelp/SwitchCell.swift
+++ Yelp/SwitchCell.swift
@@ -8,19 +8,33 @@
 
 import UIKit
 
+@objc protocol  SwitchCellDelegate  {
+    optional func switchCell(switchCell: SwitchCell, didChangeValue value: Bool)
+}
+
 class SwitchCell: UITableViewCell {
 
-    @IBOutlet weak var switchCell: UILabel!
+
+    @IBOutlet weak var switchLabel: UILabel!
     @IBOutlet weak var onSwitch: UISwitch!
+    
+    weak var delegate: SwitchCellDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        onSwitch.addTarget(self, action: "switchValueChanged", forControlEvents: UIControlEvents.ValueChanged)
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func switchValueChanged() {
+        print("switch value change")
+        delegate?.switchCell?(self, didChangeValue: onSwitch.on)
     }
 
 }
